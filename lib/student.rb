5919f411c910bbf09cc77f0bd1579469a1634673
Anthony Rurup <tcrurup@gmail.com>
2019-05-17 00:09:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,7 +29,7 @@ class Student
   
   def self.all_students_in_grade_9
     sql = <<-SQL
-      SELECT students.name
+      SELECT *
       FROM students
       WHERE students.grade = 9
     SQL
@@ -41,14 +41,14 @@ class Student
   
   def self.students_below_12th_grade
     sql = <<-SQL
-      SELECT students.name
+      SELECT *
       FROM students
       WHERE students.grade < 12
     SQL
     
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end.first
+    end
   end
   
   def save
