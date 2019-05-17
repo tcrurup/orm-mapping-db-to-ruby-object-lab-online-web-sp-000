8c72ea2bb8a55852396a53860549362dcef74753
Anthony Rurup <tcrurup@gmail.com>
2019-05-17 00:06:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,9 @@ class Student
       WHERE students.grade = 9
     SQL
     
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def self.students_below_12th_grade
