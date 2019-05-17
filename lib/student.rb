282b34b4c4050c1c6b5674d7be2c43db53f851cb
Anthony Rurup <tcrurup@gmail.com>
2019-05-17 00:14:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -74,7 +74,8 @@ class Student
   end
   
   def self.first_student_in_grade_10
-    self.first_X_students_in_grade_10(1)
+    student = self.first_X_students_in_grade_10(1)
+    student
   end
   
   def save
