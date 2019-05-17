1b9fffa917f8a6a6968760ae34ec738cc867da1b
Anthony Rurup <tcrurup@gmail.com>
2019-05-17 00:15:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,7 @@ class Student
     end
   end
   
-  def self.first_X_students_in_grade_10(x:)
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL 
       SELECT * 
       FROM students
@@ -74,7 +74,7 @@ class Student
   end
   
   def self.first_student_in_grade_10
-    self.first_X_students_in_grade_10(x:1)
+    self.first_X_students_in_grade_10(1)
   end
   
   def save
